cb36ea35ea9d8cc84f86a4b7c062333cd70250be
Hadi Ahmadi <MacPersia@gmail.com>
2018-09-01 19:59:33 +0800

    Trying to use object as the payload type... (WIP)

--- src/main/java/be/planty/agents/assistant/AgentSessionHandler.java
+++ src/main/java/be/planty/agents/assistant/AgentSessionHandler.java
@@ -1,11 +1,15 @@
 package be.planty.agents.assistant;
 
+import be.planty.models.assistant.ActionRequest;
+import be.planty.models.assistant.ActionResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 
-import java.util.AbstractMap.SimpleEntry;
+import java.lang.reflect.Type;
+
+import static be.planty.models.assistant.Constants.PAYLOAD_TYPE_KEY;
 
 public class AgentSessionHandler extends MyStompSessionHandler {
 
@@ -23,23 +27,44 @@ public class AgentSessionHandler extends MyStompSessionHandler {
     }
 
     @Override
+    public Type getPayloadType(StompHeaders headers) {
+        final String typeName = headers.getFirst(PAYLOAD_TYPE_KEY);
+        if (typeName == null)
+            return super.getPayloadType(headers);
+        try {
+            return Class.forName(typeName);
+
+        } catch (ClassNotFoundException e) {
+            logger.error(e.getMessage(), e);
+            return ActionRequest.class;
+        }
+    }
+
+    @Override
     public void handleFrame(StompHeaders headers, Object payload) {
         logger.info("Received headers: " + headers);
-        logger.info("Received payload: " + payload);
+        logger.info("Received payload: " +
+                (payload instanceof String ?
+                        payload : toPrettyJson(payload)));
+
+        final String dest = "/topic/action-responses";
         if (payload.equals("Ping!")) {
             final String response = "Pong!";
             logger.info("Sending: " + response);
-            session.send("/topic/action-responses", response);
+            session.send(dest, response);
 
-        } else if (payload instanceof SimpleEntry
-                && ((SimpleEntry) payload).getKey().equals("data")
-                && ((SimpleEntry) payload).getValue().equals("Ping!") ) {
-            final var response = new SimpleEntry<>("data", "Pong!");
+        } else if (payload instanceof ActionRequest
+                    && ((ActionRequest)payload).action.equals("Ping!")) {
+
+            final ActionResponse response = new ActionResponse(0);
             logger.info("Sending: " + response);
-            session.send("/topic/action-responses", response);
+            final StompHeaders newHeaders = new StompHeaders();
+            headers.forEach(newHeaders::put);
+            newHeaders.setDestination(dest);
+            newHeaders.set(PAYLOAD_TYPE_KEY, response.getClass().getTypeName());
+            session.send(newHeaders, response);
         }
-
-        //        if (headers.getDestination().startsWith("/user/queue/action-requests")) {
+//        if (headers.getDestination().startsWith("/user/queue/action-requests")) {
 //            session.send("/topic/action-responses", "Thank you for choosing us!");
 //        }
     }
